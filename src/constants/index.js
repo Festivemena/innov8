a090c0885f14ec40b5092c573e9fe7cbcf3e61be
Efemena <88979259+Festivemena@users.noreply.github.com>
2024-01-12 20:12:44 +0100

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -120,13 +120,13 @@ const technologies = [
 
 const experiences = [
   {
-    title: "React.js Developer",
-    company_name: "Kosbis",
+    title: "Hardware Engineer",
+    company_name: "Havitech Corps",
     icon: starbucks,
     iconBg: "#383E56",
     date: "March 2020 - April 2021",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Developing Scalable Hardware Solutions that are impactful to societal growth and development.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
       "Implementing responsive design and ensuring cross-browser compatibility.",
       "Participating in code reviews and providing constructive feedback to other developers.",
